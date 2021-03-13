8867b67a2a660d3f916df68d2cca53a4aae59e1f
Jun Yu <shockz@me.com>
2021-03-13 17:49:02 +0900

    mvc client auth complete

--- IdentityServer/Quickstart/Account/AccountOptions.cs
+++ IdentityServer/Quickstart/Account/AccountOptions.cs
@@ -6,15 +6,16 @@ using System;
 
 namespace IdentityServerHost.Quickstart.UI
 {
-    public class AccountOptions
-    {
-        public static bool AllowLocalLogin = true;
-        public static bool AllowRememberLogin = true;
-        public static TimeSpan RememberMeLoginDuration = TimeSpan.FromDays(30);
+  public class AccountOptions
+  {
+    public static bool AllowLocalLogin = true;
+    public static bool AllowRememberLogin = true;
+    public static TimeSpan RememberMeLoginDuration = TimeSpan.FromDays(30);
 
-        public static bool ShowLogoutPrompt = true;
-        public static bool AutomaticRedirectAfterSignOut = false;
+    public static bool ShowLogoutPrompt = true;
+    //public static bool AutomaticRedirectAfterSignOut = false;
+    public static bool AutomaticRedirectAfterSignOut = true;
 
-        public static string InvalidCredentialsErrorMessage = "Invalid username or password";
-    }
+    public static string InvalidCredentialsErrorMessage = "Invalid username or password";
+  }
 }
--- Movies.Client/Controllers/MoviesController.cs
+++ Movies.Client/Controllers/MoviesController.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Movies.Client.ApiServices;
 using Movies.Client.Models;
 
@@ -20,7 +25,26 @@ namespace Movies.Client.Controllers
     // GET: Movies
     public async Task<IActionResult> Index()
     {
+      LogTokenAndClaim(); // await 해도 되고, 안해도 되고, 다만 페이지 로딩 전 보고 싶을땐 await 없이...
       return View(await _movieApiService.GetMovies());
+    }
+
+    // for logging
+    public async Task LogTokenAndClaim()
+    {
+      var identityToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.IdToken);
+      Debug.WriteLine($"Identity token: {identityToken}");
+
+      foreach (var claim in User.Claims)
+      {
+        Debug.WriteLine($"Claim type: {claim.Type} - Claim value: {claim.Value}");
+      }
+    }
+
+    public async Task Logout()
+    {
+      await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+      await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
     }
 
     // GET: Movies/Details/5
