f259df11e67240611a71773bb324975cd7a3a2f7
Jun Yu <shockz@me.com>
2021-03-13 16:47:13 +0900

    identity server support login

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityModel;
+using IdentityServer4;
 using IdentityServer4.Models;
 using IdentityServer4.Test;
 
@@ -19,6 +22,30 @@ namespace IdentityServer
           new Secret("secret".Sha256())
         },
         AllowedScopes = {"movieAPI"}
+      },
+      new Client
+      {
+        ClientId = "movies_mvc_client",
+        ClientName = "Movies MVC Web App",
+        AllowedGrantTypes = GrantTypes.Code,
+        AllowRememberConsent = false,
+        RedirectUris = new List<string>()
+        {
+          "https://localhost:5002/signin-oidc" // this is client app port
+        },
+        PostLogoutRedirectUris = new List<string>()
+        {
+          "https://localhost:5002/signout-callback-oidc"
+        },
+        ClientSecrets = new List<Secret>
+        {
+          new Secret("secret".Sha256())
+        },
+        AllowedScopes = new List<string>
+        {
+          IdentityServerConstants.StandardScopes.OpenId,
+          IdentityServerConstants.StandardScopes.Profile
+        }
       }
     };
 
@@ -33,10 +60,23 @@ namespace IdentityServer
 
     public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
     {
+      new IdentityResources.OpenId(), // client scope 추가에 따른 identity resource 추가
+      new IdentityResources.Profile() // client scope 추가에 따른 identity resource 추가
     };
 
     public static List<TestUser> TestUsers => new List<TestUser>
     {
+      new TestUser
+      {
+        SubjectId = "46A73B00-6F08-4CCE-BBFA-CDC80767E33B",
+        Username = "shockz",
+        Password = "shockz",
+        Claims = new List<Claim>
+        {
+          new Claim(JwtClaimTypes.GivenName, "Jun"),
+          new Claim(JwtClaimTypes.FamilyName, "Yu")
+        }
+      }
     };
   }
 }
